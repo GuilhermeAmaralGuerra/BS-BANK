0e01ab44bab9ef87f08c982b56034d67bb23dfaf
Guilherme Amaral Guerra <guilhermeguerra267@gmail.com>
2025-02-10 15:52:17 -0300

    Retirando alguns imports desnecessários

--- monetario/src/main/java/banco/projeto/monetario/domain/Conta.java
+++ monetario/src/main/java/banco/projeto/monetario/domain/Conta.java
@@ -4,9 +4,6 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
--- monetario/src/main/java/banco/projeto/monetario/domain/Usuario.java
+++ monetario/src/main/java/banco/projeto/monetario/domain/Usuario.java
@@ -2,8 +2,6 @@ package banco.projeto.monetario.domain;
 
 import java.time.LocalDate;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
